41b1f7fbf75180e063992a9df945c610d0649e5c
UniBreakfast <unibreakfast1@gmail.com>
2024-10-01 21:47:58 +0300

    uncomment some functions to get rid of some errors

--- jsREPLmagic/REPLmagic_lib.js
+++ jsREPLmagic/REPLmagic_lib.js
@@ -6,28 +6,28 @@ log = console.log
 
 // if given a number returns a string with that number with 'px' added
 // otherwise returns what was given
-let n2px  = num => typeof num == 'number' ? num+'px' : num
+let n2px = num => typeof num == 'number' ? num + 'px' : num
 
 // if given 1-2 digit number or a valid hex character returns a CSS grey from it
-let n2col = num => typeof num == 'number' && String(num).length<3 && num>=0 ||
-  ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F']
-  .includes(num) ? '#'+num+num+num : num
+let n2col = num => typeof num == 'number' && String(num).length < 3 && num >= 0 ||
+  ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F']
+    .includes(num) ? '#' + num + num + num : num
 
 const elPro = HTMLElement.prototype
 
 elPro.evo = function evolve() {
-  this.s  = this.style
+  this.s = this.style
   this.ch = this.children
   return this
 }
 
-elPro.r  = function   remove()     { this.remove();          return this }
-elPro.e  = function    empty()     { this.innerHTML = '';    return this }
-elPro.a  = function   append(that) { this.appendChild(that); return this }
-elPro.a2 = function appendTo(that) { that.a(this);           return this }
+elPro.r = function remove() { this.remove(); return this }
+elPro.e = function empty() { this.innerHTML = ''; return this }
+elPro.a = function append(that) { this.appendChild(that); return this }
+elPro.a2 = function appendTo(that) { that.a(this); return this }
 
 let gsp = function getSetProperty(alias, prop, turnf) {
-  eval (`elPro['${alias}'] = function ${prop}(val) {
+  eval(`elPro['${alias}'] = function ${prop}(val) {
   if (val===_) return this['${prop}']
   this['${prop}'] = (${turnf}) ? (${turnf})(val) : val
   return this
@@ -35,11 +35,10 @@ let gsp = function getSetProperty(alias, prop, turnf) {
 }
 gsp('Id', 'id')
 gsp('tx', 'innerText')
-gsp('htm','innerHTML')
+gsp('htm', 'innerHTML')
 
 let gssp = function getSetStyleProperty(alias, prop, turnf) {
-  eval (`elPro['${alias}'] = function style${
-        prop.charAt(0).toUpperCase() + prop.slice(1)}(val) {
+  eval(`elPro['${alias}'] = function style${prop.charAt(0).toUpperCase() + prop.slice(1)}(val) {
   if (val===_) return this.style['${prop}']
   this.style['${prop}'] = (${turnf}) ? (${turnf})(val) : val
   return this
@@ -49,7 +48,7 @@ gssp('sdi', 'display')
 gssp('sbrs', 'borderStyle')
 
 let gsscp =
-    function getSetStyleColorProperty (alias, prop) { gssp(alias, prop, n2col) }
+  function getSetStyleColorProperty(alias, prop) { gssp(alias, prop, n2col) }
 gsscp('sb', 'background')
 gsscp('c', 'color')
 gsscp('brc', 'borderColor')
@@ -62,43 +61,47 @@ const evo = (...els) => {
   els.forEach(el => {
 
     // shortcuts for commonly used methods and groups of methods of DOM element
-    //el.s = el.style
+    el.s = el.style
     el.evo()
 
     // remove or empty element
-    //el.r = () => { el.remove();       return el }
-    //el.e = () => { el.innerHTML = ''; return el }
+    el.r = () => { el.remove();       return el }
+    el.e = () => { el.innerHTML = ''; return el }
 
     // append child and append this to that
-    //el.a  = el2 => { el.appendChild(el2); return el }
-    //el.a2 = el2 => { el2.a(el);           return el }
+    el.a  = el2 => { el.appendChild(el2); return el }
+    el.a2 = el2 => { el2.a(el);           return el }
 
     // creates set/get method for a property
     // if function given, value goes through it
-    //let sgm = (short, prop, func) => { el[short] = val => {
-    //  if (val === _) return el[prop]
-    //  el[prop] = func ? func(val) : val
-    //  return el
-    //} }
-    //sgm('Id', 'id')
-    //sgm('tx', 'innerText')
-    //sgm('htm','innerHTML')
+    let sgm = (short, prop, func) => {
+      el[short] = val => {
+        if (val === _) return el[prop]
+        el[prop] = func ? func(val) : val
+        return el
+      }
+    }
+    sgm('Id', 'id')
+    sgm('tx', 'innerText')
+    sgm('htm', 'innerHTML')
 
     // creates set/get method for a style property
     // if function given, value goes through it
-    //let sgsm = (short, prop, func) => { el.s[short] = val => {
-    //  if (val === _) return el.s[prop]
-    //  el.s[prop] = func ? func(val) : val
-    //  return el
-    //} }
-    //sgsm('di', 'display')
-    //sgsm('brs', 'borderStyle')
+    let sgsm = (short, prop, func) => {
+      el.s[short] = val => {
+        if (val === _) return el.s[prop]
+        el.s[prop] = func ? func(val) : val
+        return el
+      }
+    }
+    sgsm('di', 'display')
+    sgsm('brs', 'borderStyle')
 
     // set/get style function for color properties
-    //let sgsmc = (short, prop) => sgsm(short, prop, n2col)
-    //sgsmc('b', 'background')
-    //sgsmc('c', 'color')
-    //sgsmc('brc', 'borderColor')
+    let sgsmc = (short, prop) => sgsm(short, prop, n2col)
+    sgsmc('b', 'background')
+    sgsmc('c', 'color')
+    sgsmc('brc', 'borderColor')
 
     // set/get style function for size unit properties
     let sgsmp = (short, prop) => sgsm(short, prop, n2px)
@@ -109,18 +112,20 @@ const evo = (...els) => {
 
     // size/color shorthand for width, height, background, foreground
     el.sc = el.whbc = (w, h, b, c) => {
-      if (w===_ && h===_ && b===_ && c===_)
-        return {w: el.s.w(), h: el.s.h(), b: el.s.b(), c: el.s.c()}
-      el.s.w(w); el.s.h(h); el.s.b(b); el.s.c(c); return el }
+      if (w === _ && h === _ && b === _ && c === _)
+        return { w: el.s.w(), h: el.s.h(), b: el.s.b(), c: el.s.c() }
+      el.s.w(w); el.s.h(h); el.s.b(b); el.s.c(c); return el
+    }
 
     // color/width shorthand for element's border
     el.br = (c, w) => {
-      if (c===_ && w===_)
-        return {c: el.s.brc(), w: el.s.brw()}
-      el.s.brc(c); el.s.brw(w); el.s.brs('solid'); return el }
+      if (c === _ && w === _)
+        return { c: el.s.brc(), w: el.s.brw() }
+      el.s.brc(c); el.s.brw(w); el.s.brs('solid'); return el
+    }
 
   })
-  if (els.length==1) return els[0]
+  if (els.length == 1) return els[0]
 }
 
 evo(h, b)
@@ -132,60 +137,60 @@ nel = (tag, id) => {
   let el = d.createElement(tag).evo()
   el.s.boxSizing = 'border-box'
   if (id) el.id = id
-  return el }
-/*
+  return el
+}
+
 nel = (tag, id) => {
   let el = evo(d.createElement(tag))
   el.s.boxSizing = 'border-box'
   if (id) el.id = id
   return el }
-*/
+
 
 nel_make = tag => eval(`w['nel${tag.charAt(0)}'] = function nel${tag.charAt(0)
-                       }(id) { return nel('${tag}', id) }`)
+  }(id) { return nel('${tag}', id) }`)
 nel_make('div')
 nel_make('span')
 nel_make('input')
 nel_make('button')
 
-nel_make_r = tag => eval(`w['nel${tag.charAt(0)}r'] = function nel${
-                         tag.charAt(0)}r(id) { let el = nel('${tag
-                         }', id); el.s.borderRadius = '15%'; return el}`)
+nel_make_r = tag => eval(`w['nel${tag.charAt(0)}r'] = function nel${tag.charAt(0)}r(id) { let el = nel('${tag
+  }', id); el.s.borderRadius = '15%'; return el}`)
 nel_make_r('div')
 nel_make_r('span')
 nel_make_r('input')
 nel_make_r('button')
 
 let NAMED_COLORS =
-  ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque",
-   "Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue",
-   "Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson",
-   "Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey",
-   "DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange",
-   "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue", "DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink",
-   "DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite",
-   "ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray",
-   "Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo",
-   "Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon",
-   "LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray",
-   "LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen",
-   "LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue",
-   "LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon",
-   "MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple",
-   "MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
-   "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin",
-   "NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed",
-   "Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed",
-   "PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple",
-   "RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon",
-   "SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue",
-   "SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal",
-   "Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
-   "Yellow","YellowGreen"]
+  ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque",
+    "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue",
+    "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson",
+    "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey",
+    "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange",
+    "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink",
+    "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite",
+    "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray",
+    "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo",
+    "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon",
+    "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray",
+    "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen",
+    "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue",
+    "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon",
+    "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple",
+    "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise",
+    "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin",
+    "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed",
+    "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed",
+    "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple",
+    "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon",
+    "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue",
+    "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal",
+    "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke",
+    "Yellow", "YellowGreen"]
 
-rcol = (brigh, el, t=10) => {
+rcol = (brigh, el, t = 10) => {
   if (el) {
-    let i = setInterval(()=>el.s.background = rcol(brigh), t*100)
+    let i = setInterval(() => el.s.background = rcol(brigh), t * 100)
     el.onclick = () => {
       clearInterval(i)
       log(el.s.b())
@@ -193,30 +198,31 @@ rcol = (brigh, el, t=10) => {
     return el
   }
   // Six levels of brightness from 0 to 5, 0 being the darkest
-  if (brigh===undefined) brigh = Math.round(Math.random() * 5)
+  if (brigh === undefined) brigh = Math.round(Math.random() * 5)
   var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256]
-  var mix = [brigh*51, brigh*51, brigh*51] //51 => 255/5
+  var mix = [brigh * 51, brigh * 51, brigh * 51] //51 => 255/5
   var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]]
-    .map(x => Math.round(x/2.0))
-  return "rgb(" + mixedrgb.join(",") + ")" }
+    .map(x => Math.round(x / 2.0))
+  return "rgb(" + mixedrgb.join(",") + ")"
+}
 
 rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
-rncol = (el, t=10) => {
+rncol = (el, t = 10) => {
   if (el) {
-    let i = setInterval(()=>el.s.b(rncol()), t*100)
+    let i = setInterval(() => el.s.b(rncol()), t * 100)
     el.onclick = () => {
       clearInterval(i)
       log(el.s.b())
     }
     return el
   }
-  return NAMED_COLORS[rand(0, NAMED_COLORS.length-1)]
+  return NAMED_COLORS[rand(0, NAMED_COLORS.length - 1)]
 }
 
-rhue = (deg, el, t=10) => {
+rhue = (deg, el, t = 10) => {
   if (el) {
-    let i = setInterval(()=>el.s.background = rhue(deg), t*100)
+    let i = setInterval(() => el.s.background = rhue(deg), t * 100)
     el.onclick = () => {
       clearInterval(i)
       log(el.s.b())
@@ -227,13 +233,14 @@ rhue = (deg, el, t=10) => {
   //   30 ORANGE   150 JADE    270 VIOLET
   //   60 YELLOW   180 CYAN    300 MAGENTA
   //   90 SALAD    210 AZURE   330 ROSE
-  if (deg===undefined) deg = rand(0, 359)
+  if (deg === undefined) deg = rand(0, 359)
   colors = {
-    red:       0,   orange:   30,   yellow:   60,   salad:    90,
-    green:   120,   jade:    150,   cyan:    180,   azure:   210,
-    blue:    240,   violet:  270,   magenta: 300,   rose:    330 }
+    red: 0, orange: 30, yellow: 60, salad: 90,
+    green: 120, jade: 150, cyan: 180, azure: 210,
+    blue: 240, violet: 270, magenta: 300, rose: 330
+  }
   if (deg in colors) deg = colors[deg]
-  return "hsl("+deg+", "+ rand(10,100) +"%, "+rand(5, 95) +"%)"
+  return "hsl(" + deg + ", " + rand(10, 100) + "%, " + rand(5, 95) + "%)"
 }
 
 log('shortcuts loaded')
@@ -247,33 +254,39 @@ elp = preset => {
     case 'cc': return w.cc = neld().s.m('auto').a2(b.e())
     default:
       el = neldr().sc(100, 100, rcol(5)).s.m(2)
-      el.s.border = 'solid '+rcol(0)+' 1px'
-      return el } }
+      el.s.border = 'solid ' + rcol(0) + ' 1px'
+      return el
+  }
+}
 
 
 body_add_divs = (num, w, h) => {
   if (num > 1000) return 'too many cells, try less than 1000'
-  for (let i=0; i<num; i++) {
+  for (let i = 0; i < num; i++) {
     div = nel('div')
     div.s.display = 'inline-block'
     div.whb(w, h, rcol())
-    body.a(div) } }
+    body.a(div)
+  }
+}
 
 body_add_divs_grid = (w, h, hnum, vnum) => {
   w = w || 200; h = h || 100;
   hnum = hnum || 1; vnum = vnum || 1;
-  if (hnum*vnum > 1000) return 'too many cells, try less than 1000'
+  if (hnum * vnum > 1000) return 'too many cells, try less than 1000'
   cont = nel('div')
   cont.whb(w, h, 'black')
   cont.s.display = 'grid'
   let gridTemplate = ''
   let i
-  for (i=0; i<vnum; i++) gridTemplate += ' auto'
+  for (i = 0; i < vnum; i++) gridTemplate += ' auto'
   gridTemplate += ' /'
-  for (i=0; i<hnum; i++) gridTemplate += ' auto'
+  for (i = 0; i < hnum; i++) gridTemplate += ' auto'
   cont.s.gridTemplate = gridTemplate
   body.a(cont)
-  for (i=0; i<hnum*vnum; i++) {
+  for (i = 0; i < hnum * vnum; i++) {
     div = nel('div')
     div.whb('100%', '100%', rcol(5))
-    cont.a(div) } }
+    cont.a(div)
+  }
+}
--- jsREPLmagic/tests.js
+++ jsREPLmagic/tests.js
@@ -1,5 +1,5 @@
 
-(( tests )=>{
+((tests) => {
 
   let log = console.log
   let Tnum = 1
@@ -8,69 +8,72 @@
   // like this:       {b: 1, c: [4, "def"]}
   // instead of this: {"b":1,"c":[4,"def"]}
   rdbl = (value, unpack) => {
-    if (typeof value == 'object') { value = JSON.stringify(value).split(':')
-      .map((item,index,arr) => (index<arr.length-1) ? item.slice(0, -1) : item)
-      .map((item,index,arr) => {
-        let quoteAt = item.lastIndexOf('"')
-        return (index<arr.length-1) ? item.substring(0, quoteAt) +
-          item.substring(quoteAt+1) : item
-      })
-      .join(': ').replace(/,/g, ', ')
-      value = unpack ? value.slice(1,-1) : value
-      return value!='null' ? value : ''
+    if (typeof value == 'object') {
+      value = JSON.stringify(value).split(':')
+        .map((item, index, arr) => (index < arr.length - 1) ? item.slice(0, -1) : item)
+        .map((item, index, arr) => {
+          let quoteAt = item.lastIndexOf('"')
+          return (index < arr.length - 1) ? item.substring(0, quoteAt) +
+            item.substring(quoteAt + 1) : item
+        })
+        .join(': ').replace(/,/g, ', ')
+      value = unpack ? value.slice(1, -1) : value
+      return value != 'null' ? value : ''
     }
-    else if (typeof value == 'string') return "'"+value+"'"
+    else if (typeof value == 'string') return "'" + value + "'"
     else return value
   }
-  log(rdbl([undefined],1))
+  log(rdbl([undefined], 1))
 
   tFuncResult = (func, args, result, arg_str, res_str) => {
     args = Array.isArray(args) ? args : [args]
-    log(`Test ${Tnum++}:  ${func.name}(${arg_str||rdbl(args,1)}) = ${
-        res_str||rdbl(result)}    ` +
-        (func.apply(this,args)==result||
-        JSON.stringify(func.apply(this,args))==JSON.stringify(result) ?
-                                                          'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${func.name}(${arg_str || rdbl(args, 1)}) = ${res_str || rdbl(result)}    ` +
+      (func.apply(this, args) == result ||
+        JSON.stringify(func.apply(this, args)) == JSON.stringify(result) ?
+        'YES!' : 'NO...'))
+  }
   tFuncProbe = (func, args, probe_func, arg_str) => {
     args = Array.isArray(args) ? args : [args]
-    log(`Test ${Tnum++}:  ${func.name}(${arg_str||rdbl(args,1)});  ${
-        probe_func.name}()    ` + (probe_func(func.apply(this, args)||args) ?
-                                                          'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${func.name}(${arg_str || rdbl(args, 1)});  ${probe_func.name}()    ` + (probe_func(func.apply(this, args) || args) ?
+        'YES!' : 'NO...'))
+  }
   tFuncAssert = (func, args, assert_code, arg_str, asc_str) => {
     let arg = args = Array.isArray(args) ? args : [args]
     let result = func.apply(this, args)
-    log(`Test ${Tnum++}:  ${func.name}(${arg_str||rdbl(args,1)});  ${
-        asc_str||assert_code}    ` + (eval(assert_code) ? 'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${func.name}(${arg_str || rdbl(args, 1)});  ${asc_str || assert_code}    ` + (eval(assert_code) ? 'YES!' : 'NO...'))
+  }
 
   tMethResult = (obj, meth, args, result, obj_str, arg_str, res_str) => {
     if (!obj[meth]) return log(`no ${meth} method on ${obj_str} found!`)
     args = Array.isArray(args) ? args : [args]
-    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str||rdbl(args,1)}) = ${
-        res_str||rdbl(result)}    ` + (obj[meth].apply(obj,args)==result||
-        JSON.stringify(obj[meth].apply(obj,args))==JSON.stringify(result) ?
-                                                          'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str || rdbl(args, 1)}) = ${res_str || rdbl(result)}    ` + (obj[meth].apply(obj, args) == result ||
+        JSON.stringify(obj[meth].apply(obj, args)) == JSON.stringify(result) ?
+        'YES!' : 'NO...'))
+  }
   tMethProbe = (obj, meth, args, probe_func, obj_str, arg_str) => {
     if (!obj[meth]) return log(`no ${meth} method on ${obj_str} found!`)
     args = Array.isArray(args) ? args : [args]
-    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str||rdbl(args,1)});  ${
-        probe_func.name}()    `+(probe_func(obj,args,obj[meth].apply(obj,args)||
-                    (typeof args=='object'?args:obj)) ?   'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str || rdbl(args, 1)});  ${probe_func.name}()    ` + (probe_func(obj, args, obj[meth].apply(obj, args) ||
+      (typeof args == 'object' ? args : obj)) ? 'YES!' : 'NO...'))
+  }
   tMethAssert = (obj, meth, args, assert_code, obj_str, arg_str, asc_str) => {
     if (!obj[meth]) return log(`no ${meth} method on ${obj_str} found!`)
     let arg = args = Array.isArray(args) ? args : [args]
-    let result = obj[meth].apply(obj,args)
-    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str||rdbl(args,1)
-        });  ${asc_str||assert_code}    ` + (eval(assert_code) ?
-                                                          'YES!' : 'NO...')) }
+    let result = obj[meth].apply(obj, args)
+    log(`Test ${Tnum++}:  ${obj_str}.${meth}(${arg_str || rdbl(args, 1)
+      });  ${asc_str || assert_code}    ` + (eval(assert_code) ?
+        'YES!' : 'NO...'))
+  }
 
   tEval = (eval_code, eval_str) => {
-    log(`Test ${Tnum++}:  ${eval_str||eval_code}    ` + (eval(eval_code) ?
-                                                          'YES!' : 'NO...')) }
+    log(`Test ${Tnum++}:  ${eval_str || eval_code}    ` + (eval(eval_code) ?
+      'YES!' : 'NO...'))
+  }
 
   //Test:  basic short aliases
-  tEval("w == window && d == document && "+
-        "b == body && b == d.body && h == head && h == d.head && " +
-        "_ === undefined")
+  tEval("w == window && d == document && " +
+    "b == body && b == d.body && h == head && h == d.head && " +
+    "_ === undefined")
   log('')
 
   //Test:  n2px(45) = '45px'
@@ -78,7 +81,7 @@
   //Test:  n2px('45px') = '45px'
   tFuncResult(n2px, '45px', '45px')
   //Test:  n2px({a: 45}) = {a: 45}
-  tFuncResult(n2px, {a: 45}, {a: 45})
+  tFuncResult(n2px, { a: 45 }, { a: 45 })
   log('')
 
   //Test:  n2col(45) = '#454545'
@@ -93,71 +96,60 @@
 
   //Test: evo(<div>) assigns <div>.s == <div>.style
   tFuncProbe(evo, d.createElement('div'),
-            function dot_s_for_style(res) { return res.s == res.style },
-            "<div>")
+    function dot_s_for_style(res) { return res.s == res.style },
+    "<div>")
 
   //Test: nel('div') returns <div>
   tFuncProbe(nel, 'div',
-            function div_created(res) { return res.tagName == 'DIV' })
+    function div_created(res) { return res.tagName == 'DIV' })
   //Test: nel('div', 'myDiv') returns <div id=myDiv>
   tFuncProbe(nel, ['div', 'myDiv'],
-            function div_w_id_created(res) { return res.id == 'myDiv' })
+    function div_w_id_created(res) { return res.id == 'myDiv' })
   //Test: nel_make('div') declares neld(id)
   tFuncAssert(nel_make, 'div', "typeof w.neld == 'function'")
   //Test: neld('myDiv') returns <div>
-  tFuncProbe(neld, 'myDiv', function div_w_id_created(res)
-            { return res.tagName == 'DIV' && res.id == 'myDiv' })
+  tFuncProbe(neld, 'myDiv', function div_w_id_created(res) { return res.tagName == 'DIV' && res.id == 'myDiv' })
   //Test: neldr('myDiv') returns <div> with rounded corners
-  tFuncProbe(neldr, _, function div_w_rounded_corners(res)
-            { return res.s.borderRadius == '15%' })
+  tFuncProbe(neldr, _, function div_w_rounded_corners(res) { return res.s.borderRadius == '15%' })
 
   //Test: evo([<div>, <div>]) -> works on multiple elements at once
   tFuncProbe(evo, [neld(), neld()],
-            function multiple_elements_at_once(res)
-              { return res[0].s == res[0].style && res[1].s == res[1].style },
-            "[<div>,<div>]")
+    function multiple_elements_at_once(res) { return res[0].s == res[0].style && res[1].s == res[1].style },
+    "[<div>,<div>]")
   log('')
 
   //Test: el1.a(el2) -> appends el2 to el1
   tMethProbe(neld('out'), 'a', neld('in'),
-             function appended_a_child(obj)
-             { return obj.children[0].id == 'in' },
-             '<div id=out>', '<div id=in>')
+    function appended_a_child(obj) { return obj.children[0].id == 'in' },
+    '<div id=out>', '<div id=in>')
   //Test: el1.a2(el2) -> appends el1 to el2
   tMethProbe(neld('in'), 'a2', neld('out'),
-             function appended_as_a_child(obj, args)
-               { return obj.parentElement == args[0] },
-             '<div id=in>', '<div id=out>')
+    function appended_as_a_child(obj, args) { return obj.parentElement == args[0] },
+    '<div id=in>', '<div id=out>')
 
   //Test: el.tx('text') -> sets innerText of an element
   tMethProbe(neld(), 'tx', 'text',
-             function sets_innerText_of_an_element(obj, args)
-               { return obj.innerText == args[0] },
-             '<div>')
+    function sets_innerText_of_an_element(obj, args) { return obj.innerText == args[0] },
+    '<div>')
   //Test: el.htm('<p>text</p>') -> sets innerHTML of an element
   tMethProbe(neld(), 'htm', '<p>text</p>',
-             function sets_innerHTML_of_an_element(obj, args)
-               { return obj.innerHTML == args[0] },
-             '<div>')
+    function sets_innerHTML_of_an_element(obj, args) { return obj.innerHTML == args[0] },
+    '<div>')
   //Test: el.r() -> removes element from DOM
   tMethProbe(neld().a2(b), 'r', _,
-             function removes_element_from_DOM(obj) { return !b.contains(obj) },
-             '<div>')
+    function removes_element_from_DOM(obj) { return !b.contains(obj) },
+    '<div>')
   //Test: el.e() -> empties element
   tMethProbe(neld().tx('text').a(neld()), 'e', _,
-             function empties_element(obj) { return !obj.htm() },
-             '<div>')
+    function empties_element(obj) { return !obj.htm() },
+    '<div>')
   log('')
 
   //Test: el.s.c(color) -> sets color
   tMethProbe(neld().s, 'c', 'red',
-             function sets_color(obj) { return obj.c()=='red' }, '<div>')
+    function sets_color(obj) { return obj.c() == 'red' }, '<div>')
   //Test: el.sc(width, height, background, color) -> sets common properties
   tMethProbe(neld(), 'sc', [20, 10, 3, 'teal'],
-             function sets_size_and_colors(obj, args)
-               { return obj.s.c() == 'teal'}, "<div>")
-
-
+    function sets_size_and_colors(obj, args) { return obj.s.c() == 'teal' }, "<div>")
 
 })(`tests`)
-
